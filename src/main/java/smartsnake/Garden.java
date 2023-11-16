36d8e8b1258e15aca850d80f56ca0918f4bae82b
chanacham <cchambre@student.touro.edu>
2023-11-16 16:43:04 -0500

    intial discussion to open PR

--- src/main/java/smartsnake/Garden.java
+++ src/main/java/smartsnake/Garden.java
@@ -16,18 +16,23 @@ public class Garden {
     }
 
     private void createSnake() {
+        //created with a fixed length and width, in a fixed position at the center of the screen
 
     }
 
     private void createFood() {
+        //created initially with a fixed position in front of the snake
 
     }
 
     public Snake getSnake() {
+        //bring the snake into this class from where it was created in the Snake class
+        //create a Snake object here
         return null;
     }
 
     public Food getFood() {
+        //create a Food object to bring the Food into this class
         return null;
     }
 
@@ -39,6 +44,10 @@ public class Garden {
      * @return true if the simulation should continue, otherwise false
      */
     public boolean tick() {
+        //Question: unsure if the tick method is supposed to have calculations of snake moving or turning?
+        //every tick will be 1. a movement of the snake
+                            //2. consume any food in its path and then spawn more food
+                            //3. end the game if the snake hits itself or the wall
         return false;
     }
 
@@ -46,6 +55,7 @@ public class Garden {
      * @return the number of times tick() was called successfully.
      */
     public int getTurns() {
+        //This goes back to our previous question of where does the turning happen? in the tick method?
         return 0;
     }
 

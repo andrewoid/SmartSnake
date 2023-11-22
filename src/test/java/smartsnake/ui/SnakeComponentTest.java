fd616867934c6d182129d0dfa6659cca05c92338
ElkySchles <elbrsc523@gmail.com>
2023-11-22 14:10:39 -0500

    drawSnake test passed + previous comments corrected

--- src/test/java/smartsnake/ui/SnakeComponentTest.java
+++ src/test/java/smartsnake/ui/SnakeComponentTest.java
@@ -7,10 +7,13 @@ import smartsnake.Garden;
 import smartsnake.Snake;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
+//import static smartsnake.ui.SnakeComponent.SQUARE_SIZE;
 
 class SnakeComponentTest {
     //mock garden, snake, and regular food, mock graphics
@@ -18,7 +21,13 @@ class SnakeComponentTest {
     private final Garden garden = mock();
     private final Graphics graphics = mock();
     private final SnakeComponent snakeComponent = new SnakeComponent(garden);
-    private final Food food = new Food();
+    private final Food food = new Food(2, 3);
+    private final Snake snake = mock();
+    private static final int SQUARE_SIZE = 10;
+    List<Point> segments = new ArrayList<>(List.of(
+            new Point(mock())
+    ));
+
 
 
     /*@Test
@@ -29,7 +38,7 @@ class SnakeComponentTest {
     @Test
     void drawFood() {
         //given
-        int SQUARE_SIZE = 10;
+
         doReturn(food).when(garden).getFood();
 
         //when
@@ -39,5 +48,20 @@ class SnakeComponentTest {
         Mockito.verify(graphics).fillRect(food.x, food.y, SQUARE_SIZE, SQUARE_SIZE);
 
 
+    }
+
+    @Test
+    void drawSnake() {
+        //given
+
+        doReturn(snake).when(garden).getSnake();
+        doReturn(segments).when(snake).getSegments();
+        Point point = segments.get(0);
+
+        //when
+        snakeComponent.drawSnake(graphics, snake);
+
+        //then
+        Mockito.verify(graphics).fillRect(point.x, point.y, SQUARE_SIZE, SQUARE_SIZE);
     }
 }

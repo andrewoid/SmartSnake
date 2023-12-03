537adabb0bc8a36ba0a2316b0c44122fd639198f
lkenigsb <libbykenigsberg@gmail.com>
2023-12-03 11:42:16 -0500

    updated AStarBrain to work

--- src/main/java/smartsnake/astar/kenigsberg/AstarBrain.java
+++ src/main/java/smartsnake/astar/kenigsberg/AstarBrain.java
@@ -16,10 +16,6 @@ public class AstarBrain implements Brain {
      */
     @Override
     public Direction move(Snake snake, Food food, Garden garden) {
-        // Notes:
-        // look at whole path and then determine the direction
-        // loop = while there are still open nodes
-        // Open will start with the node at the head of the snake
 
         ArrayList<Node> openNodes = new ArrayList<>();
         ArrayList<Node> closedNodes = new ArrayList<>();
@@ -27,114 +23,90 @@ public class AstarBrain implements Brain {
         Node startNode = new Node(snake.getHeadLocation(), food.getLocation());
         openNodes.add(startNode);
 
-        List<Point> snakeSegments = snake.getSegments();
-        for (int i = 1; i < snakeSegments.size(); i++) {
-            closedNodes.add(new Node(snakeSegments.get(i).getLocation(), food));
-        }
+        setSnakeToClosed(snake, food, closedNodes);
 
         while (!openNodes.isEmpty()) {
             Node current = openNodes.get(0);
-            double lowestfcost = openNodes.get(0).getCost();
-            for (Node open : openNodes) {
-                if (open.getCost() < lowestfcost) {
-                    lowestfcost = open.getCost();
-                    current = open;
-                }
-            }
+            current = setLowestFCost(openNodes, current);
 
             openNodes.remove(current);
             closedNodes.add(current);
 
-            if (current.getLocation().equals(food)) {
+            Point currentLocation = current.getLocation();
+            if (currentLocation.equals(food)) {
                 return current.getDirectionFromStart();
             }
 
             ArrayList<Node> neighborsOfCurrent = new ArrayList<>();
 
-            if (!openNodes.contains(new Node(new Point(current.getLocation().x + 1, current.getLocation().y), food))) {
-                Node rightNode = new Node(new Point(current.getLocation().x + 1, current.getLocation().y), food);
+            int indexRight = openNodes.indexOf(new Node(new Point(
+                    currentLocation.x + 1, currentLocation.y), food));
+            if (indexRight == -1) {
+                Node rightNode = new Node(new Point(currentLocation.x + 1, currentLocation.y), food);
                 rightNode.setParent(current, Direction.Right);
                 neighborsOfCurrent.add(rightNode);
             } else {
-                int index = openNodes.indexOf(new Node(new Point(
-                        current.getLocation().x + 1, current.getLocation().y), food));
-                neighborsOfCurrent.add(openNodes.get(index));
+                neighborsOfCurrent.add(openNodes.get(indexRight));
             }
 
-            if (!openNodes.contains(new Node(new Point(current.getLocation().x, current.getLocation().y - 1), food))) {
-                Node aboveNode = new Node(new Point(current.getLocation().x, current.getLocation().y - 1), food);
+            int indexAbove = openNodes.indexOf(new Node(new Point(
+                    currentLocation.x, currentLocation.y - 1), food));
+            if (indexAbove == -1) {
+                Node aboveNode = new Node(new Point(currentLocation.x, currentLocation.y - 1), food);
                 aboveNode.setParent(current, Direction.Up);
                 neighborsOfCurrent.add(aboveNode);
             } else {
-                int index = openNodes.indexOf(new Node(new Point(
-                        current.getLocation().x, current.getLocation().y - 1), food));
-                neighborsOfCurrent.add(openNodes.get(index));
+                neighborsOfCurrent.add(openNodes.get(indexAbove));
             }
 
-            if (!openNodes.contains(new Node(new Point(current.getLocation().x - 1, current.getLocation().y), food))) {
+            int indexLeft = openNodes.indexOf(new Node(new Point(
+                    current.getLocation().x - 1, current.getLocation().y), food));
+            if (indexLeft == -1) {
                 Node leftNode = new Node(new Point(current.getLocation().x - 1, current.getLocation().y), food);
                 leftNode.setParent(current, Direction.Left);
                 neighborsOfCurrent.add(leftNode);
             } else {
-                int index = openNodes.indexOf(new Node(new Point(
-                        current.getLocation().x - 1, current.getLocation().y), food));
-                neighborsOfCurrent.add(openNodes.get(index));
+                neighborsOfCurrent.add(openNodes.get(indexLeft));
             }
 
-            if (!openNodes.contains(new Node(new Point(current.getLocation().x, current.getLocation().y + 1), food))) {
-                Node belowNode = new Node(new Point(current.getLocation().x, current.getLocation().y + 1), food);
+            int indexBelow = openNodes.indexOf(new Node(new Point(
+                    currentLocation.x, currentLocation.y + 1), food));
+            if (indexBelow == -1) {
+                Node belowNode = new Node(new Point(currentLocation.x, currentLocation.y + 1), food);
                 belowNode.setParent(current, Direction.Down);
                 neighborsOfCurrent.add(belowNode);
             } else {
-                int index = openNodes.indexOf(new Node(new Point(
-                        current.getLocation().x, current.getLocation().y + 1), food));
-                neighborsOfCurrent.add(openNodes.get(index));
+                neighborsOfCurrent.add(openNodes.get(indexBelow));
             }
 
             for (Node neighbor : neighborsOfCurrent) {
-                if (closedNodes.contains(neighbor)) {
-                    break;
-                } else {
-                    if (neighbor.setParent(current, neighbor.getParentDirection()) || !openNodes.contains(neighbor)) {
+                if (!closedNodes.contains(neighbor)) {
+                    if (!openNodes.contains(neighbor)) {
                         neighbor.setParent(current, neighbor.getParentDirection());
-                        if (!openNodes.contains(neighbor)) {
-                            openNodes.add(neighbor);
-                        }
+                        openNodes.add(neighbor);
+
                     }
                 }
             }
         }
         return null;
     }
-}
 
-        /*
-        while (!snake.intersectsItself() && !snake.intersectsHead(food) && !snake.intersects(food)) {
-            Node rightNode = new Node(new Point(startNode.getLocation().x + 1, startNode.getLocation().y), food);
-            double rightCost = rightNode.getCost();
-
-            Node aboveNode = new Node(new Point(startNode.getLocation().x, startNode.getLocation().y - 1), food);
-            double aboveCost = aboveNode.getCost();
-
-            Node leftNode = new Node(new Point(startNode.getLocation().x - 1, startNode.getLocation().y), food);
-            double leftCost = leftNode.getCost();
-
-            Node belowNode = new Node(new Point(startNode.getLocation().x, startNode.getLocation().y + 1), food);
-            double belowCost = belowNode.getCost();
-
-            if (rightCost < aboveCost && rightCost < leftCost && rightCost < belowCost) {
-                return Direction.Right;
-            } else if (aboveCost < rightCost && aboveCost < leftCost && aboveCost < belowCost) {
-                return Direction.Up;
-            } else if (leftCost < rightCost && leftCost < aboveCost && leftCost < belowCost) {
-                return Direction.Left;
-            } else if (belowCost < rightCost && belowCost < aboveCost && belowCost < leftCost) {
-                return Direction.Down;
+    private static Node setLowestFCost(ArrayList<Node> openNodes, Node current) {
+        double lowestfcost = openNodes.get(0).getCost();
+        for (Node open : openNodes) {
+            if (open.getCost() < lowestfcost) {
+                lowestfcost = open.getCost();
+                current = open;
             }
         }
-        return null;
+        return current;
+    }
 
+    private static void setSnakeToClosed(Snake snake, Food food, ArrayList<Node> closedNodes) {
+        List<Point> snakeSegments = snake.getSegments();
+        for (int i = 1; i < snakeSegments.size(); i++) {
+            closedNodes.add(new Node(snakeSegments.get(i), food));
+        }
     }
 }
-         */
-

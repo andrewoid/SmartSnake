0578ad6c0b349c8e005a20a3771cdb9ada977806
lkenigsb <libbykenigsberg@gmail.com>
2023-12-04 08:34:17 -0500

    updated setParent in AstarBrain

--- src/main/java/smartsnake/astar/kenigsberg/AstarBrain.java
+++ src/main/java/smartsnake/astar/kenigsberg/AstarBrain.java
@@ -33,48 +33,44 @@ public class AstarBrain implements Brain {
 
             Node rightNode = new Node(new Point(currentLocation.x + 1, currentLocation.y), food);
             if (!closedNodes.contains(rightNode)) {
-                if (rightNode.setParent(current, Direction.Right) || !openNodes.contains(rightNode)) {
-                    {
-                        rightNode.setParent(current, Direction.Right);
-                        if (!openNodes.contains(rightNode)) {
-                            openNodes.add(rightNode);
-                        }
-                    }
+                setParentInBrain(rightNode, current, Direction.Right);
+                if (!openNodes.contains(rightNode)) {
+                    openNodes.add(rightNode);
                 }
             }
+
 
             Node aboveNode = new Node(new Point(currentLocation.x, currentLocation.y - 1), food);
             if (!closedNodes.contains(aboveNode)) {
-                if (rightNode.setParent(current, Direction.Up) || !openNodes.contains(aboveNode)) {
-                    aboveNode.setParent(current, Direction.Up);
-                    if (!openNodes.contains(aboveNode)) {
-                        openNodes.add(aboveNode);
-                    }
+                setParentInBrain(aboveNode, current, Direction.Up);
+                if (!openNodes.contains(aboveNode)) {
+                    openNodes.add(aboveNode);
                 }
             }
 
+
             Node leftNode = new Node(new Point(current.getLocation().x - 1, current.getLocation().y), food);
             if (!closedNodes.contains(leftNode)) {
-                if (rightNode.setParent(current, Direction.Left) || !openNodes.contains(leftNode)) {
-                    leftNode.setParent(current, Direction.Left);
-                    if (!openNodes.contains(leftNode)) {
-                        openNodes.add(leftNode);
-                    }
+                setParentInBrain(leftNode, current, Direction.Left);
+                if (!openNodes.contains(leftNode)) {
+                    openNodes.add(leftNode);
                 }
+
             }
 
             Node belowNode = new Node(new Point(currentLocation.x, currentLocation.y + 1), food);
             if (!closedNodes.contains(belowNode)) {
-                if (rightNode.setParent(current, Direction.Down) || !openNodes.contains(belowNode)) {
-                    belowNode.setParent(current, Direction.Down);
-                    if (!openNodes.contains(belowNode)) {
-                        openNodes.add(belowNode);
-                    }
+                setParentInBrain(belowNode, current, Direction.Down);
+                if (!openNodes.contains(belowNode)) {
+                    openNodes.add(belowNode);
                 }
             }
-
         }
         return null;
+    }
+
+    public void setParentInBrain(Node currentNode, Node parent, Direction direction) {
+        currentNode.setParent(parent, direction);
     }
 
     public Node getLowestCost(ArrayList<Node> openNodes) {

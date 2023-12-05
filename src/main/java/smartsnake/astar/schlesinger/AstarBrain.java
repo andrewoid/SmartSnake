c42417d4467ca0f2800d60978e66ad6d3591504f
ElkySchles <elbrsc523@gmail.com>
2023-12-05 13:14:27 -0500

    Working A Star with many bugs

--- src/main/java/smartsnake/astar/schlesinger/AstarBrain.java
+++ src/main/java/smartsnake/astar/schlesinger/AstarBrain.java
@@ -4,6 +4,7 @@ import smartsnake.*;
 import smartsnake.astar.Node;
 
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
@@ -22,61 +23,93 @@ public class AstarBrain implements Brain {
         //Start node
         Node startNode = new Node(startNodeLoc, food);
 
-        //Setting my current, which starts off as start node
-        Node current = startNode;
-
-        //setting the top, bottom, right, left points
-        Point top = new Point(startNodeLoc.x, startNodeLoc.y + 1);
-        Point bottom = new Point(startNodeLoc.x, startNodeLoc.y - 1);
-        Point right = new Point(startNodeLoc.x + 1, startNodeLoc.y);
-        Point left = new Point(startNodeLoc.x - 1, startNodeLoc.y);
 
         //setting the open and close-set
-        Set<Node> open = new HashSet<>();
-        Set<Point> closed = new HashSet<>();
+        ArrayList<Node> open = new ArrayList<>();
+        ArrayList<Point> closed = new ArrayList<>();
+        ArrayList<Node> neighbors = new ArrayList<>();
 
-        //adding the startNodeLoc to close
         open.add(startNode);
         closed.addAll(snake.getSegments());
+        closed.remove(snake.getHeadLocation());
 
         //turning the neighbor points into nodes
-        Node topNode = new Node(top, food);
-        Node bottomNode = new Node(bottom, food);
-        Node rightNode = new Node(right, food);
-        Node leftNode = new Node(left, food);
-        //Adding the current node to open
-        open.add(startNode);
+
 
         while (!open.isEmpty()) {
+            Node lowestCost = open.get(0);
             //iterating through open to find the lowest f cost
             for (Node node : open) {
-                if (node.getCost() < current.getCost() || node.getCost() == current.getCost()) {
-                    current = node;
+                if (node.getCost() <= lowestCost.getCost()) {
+                    lowestCost = node;
                 }
             }
+            Node current = lowestCost;
+            open.remove(current);
             closed.add(current.getLocation());
+
             //if the snake head has reached the food
-            if (current.getLocation().equals(food.getLocation())) {
+            if (current.getLocation().equals(food)) {
                 return current.getDirectionFromStart();
             }
+
+            Point currentLocation = current.getLocation();
+            //setting the top, bottom, right, left points
+            Point top = new Point(currentLocation.x, currentLocation.y + 1);
+            Point bottom = new Point(currentLocation.x, currentLocation.y - 1);
+            Point right = new Point(currentLocation.x + 1, currentLocation.y);
+            Point left = new Point(currentLocation.x - 1, currentLocation.y);
+
+            Node topNode = new Node(top, food);
+            Node bottomNode = new Node(bottom, food);
+            Node rightNode = new Node(right, food);
+            Node leftNode = new Node(left, food);
+
+            neighbors.add(topNode);
+            neighbors.add(bottomNode);
+            neighbors.add(rightNode);
+            neighbors.add(leftNode);
+
+
             //what is being traversed
-            for (Node neighbor : open) {
+            for (Node neighbor : neighbors) {
                 if (closed.contains(neighbor.getLocation())) {
                     continue;
                 }
-                if (neighbor.getFromStart() < current.getFromStart() || !(open.contains(neighbor))) {
-                    open.add(neighbor);
+
+                //Setting new parents to neighbors that are already in open
+                if (open.contains(neighbor)) {
+
+                    if (neighbor.equals(topNode)) {
+                        open.get(open.indexOf(neighbor)).setParent(current, Direction.Down);
+                    }
+                    if (neighbor.equals(bottomNode)) {
+                        open.get(open.indexOf(neighbor)).setParent(current, Direction.Up);
+                    }
+                    if (neighbor.equals(rightNode)) {
+                        open.get(open.indexOf(neighbor)).setParent(current, Direction.Left);
+                    }
+                    if (neighbor.equals(leftNode)) {
+                        open.get(open.indexOf(neighbor)).setParent(current, Direction.Right);
+                    }
+
+                } else {
+
                     if (neighbor.equals(topNode)) {
                         neighbor.setParent(current, Direction.Down);
+                        open.add(neighbor);
                     }
                     if (neighbor.equals(bottomNode)) {
                         neighbor.setParent(current, Direction.Up);
+                        open.add(neighbor);
                     }
                     if (neighbor.equals(rightNode)) {
                         neighbor.setParent(current, Direction.Left);
+                        open.add(neighbor);
                     }
                     if (neighbor.equals(leftNode)) {
                         neighbor.setParent(current, Direction.Right);
+                        open.add(neighbor);
                     }
                 }
             }

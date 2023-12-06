454400122ada6cb5796e1a55c96e6d1fd3173dd5
MC-Student <meiratova123@gmail.com>
2023-12-05 23:03:41 -0500

    Completed AstarBrain move() method, created tests, missing one test, current tests all pass except for move() - it takes too long. Seems to be a bug in the move() method?

---
+++ src/main/java/smartsnake/astar/cohen/AstarBrain.java
@@ -0,0 +1,98 @@
+package smartsnake.astar.cohen;
+
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.VisibleForTesting;
+import smartsnake.*;
+import smartsnake.astar.Node;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AstarBrain implements Brain
+{
+    @Override
+    public @Nullable Direction move(Snake snake, Food food, Garden garden)
+    {
+        List<Node> open = new ArrayList<>();
+        List<Node> closed = new ArrayList<>();
+        Node start = new Node(snake.getHeadLocation(), food.getLocation());
+        open.add(start);
+
+        closeSnakeNodes(snake, food, closed);
+
+        while(!open.isEmpty())
+        {
+            Node current = getLowestCostNode(open);
+            open.remove(current);
+            closed.add(current);
+
+            if(current.getLocation() == food.getLocation())
+            {
+                return current.getDirectionFromStart();
+            }
+
+            Node right = new Node(new Point(current.getLocation().x + 1, current.getLocation().y), food.getLocation());
+            Node left = new Node(new Point(current.getLocation().x - 1, current.getLocation().y), food.getLocation());
+            Node up = new Node(new Point(current.getLocation().x, current.getLocation().y - 1), food.getLocation());
+            Node down = new Node(new Point(current.getLocation().x, current.getLocation().y + 1), food.getLocation());
+
+            List<Node> neighbors = new ArrayList<>(List.of(left, right, up, down));
+            List<String> directions = new ArrayList<>(List.of("Left", "Right", "Up", "Down"));
+
+            for(Node neighbor: neighbors)
+            {
+                if(!closed.contains(neighbor))
+                {
+                    if(!open.contains(neighbor)) //neighbor.getFromEnd() < current.getFromEnd() ||
+                    {
+                        if(!open.contains(neighbor))
+                        {
+                            open.add(neighbor);
+                        }
+                        else
+                        {
+                            neighbor = open.get(open.indexOf(neighbor));
+                        }
+                        updateNodeParent(current, neighbor, directions.get(neighbors.indexOf(neighbor)));
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    @VisibleForTesting
+    public static void updateNodeParent(Node current, Node neighbor, String direction)
+    {
+        neighbor.setParent(current, Direction.valueOf(direction));
+    }
+
+    @VisibleForTesting
+    public void closeSnakeNodes(Snake snake, Food food, List<Node>closed)
+    {
+        List<Point>snakePoints = snake.getSegments();
+        for (int i = 1; i < snakePoints.size(); i++)
+        {
+            closed.add(new Node(snakePoints.get(i).getLocation(), food.getLocation()));
+        }
+    }
+
+    @VisibleForTesting
+    public Node getLowestCostNode(List<Node> open)
+    {
+        int minCostIndex = 0;
+        Node lowest = open.get(minCostIndex);
+
+        for (int i = 0; i < open.size(); i++)
+        {
+            Node point = open.get(i);
+            if(point.getCost() < lowest.getCost())
+            {
+                minCostIndex = i;
+                lowest = open.get(minCostIndex);
+            }
+        }
+        return lowest;
+    }
+}
---
+++ src/test/java/smartsnake/astar/cohen/AstarBrainTest.java
@@ -0,0 +1,70 @@
+package smartsnake.astar.cohen;
+
+import org.junit.jupiter.api.Test;
+import smartsnake.Direction;
+import smartsnake.Food;
+import smartsnake.Garden;
+import smartsnake.Snake;
+import smartsnake.astar.Node;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AstarBrainTest
+{
+    private final Point point = new Point(10, 10);
+    Snake snake = new Snake();
+    Food food = new Food(point);
+    Garden garden = new Garden(40, 30);
+    AstarBrain brain = new AstarBrain();
+    @Test
+    public void move()
+    {
+        //Given above objects
+
+        //When
+        Direction direction = brain.move(snake, food, garden);
+
+        //Then
+        assertEquals(Direction.Up, direction);
+    }
+
+    @Test
+    public void updateNodeParent()
+    {
+
+    }
+
+    @Test
+    public void closeSnakeNodes()
+    {
+        //Given above objects and
+        List<Node> closed = new ArrayList<>();
+
+        //When
+        brain.closeSnakeNodes(snake, food, closed);
+
+        //Then
+        for (int i = 1; i < snake.getSegments().size(); i++)
+        {
+            assertEquals(snake.getSegments().get(i).getLocation(), closed.get(i-1).getLocation());
+        }
+    }
+
+    @Test
+    public void getLowestCostNode()
+    {
+        //Given
+        List<Node> open = new ArrayList<>(List.of(new Node(new Point(12, 5), food),
+                                                  new Node(new Point(15, 5), food)));
+
+        //When
+        Node lowestCost = brain.getLowestCostNode(open);
+
+        //Then
+        assertEquals(open.get(0).getLocation(), lowestCost.getLocation());
+    }
+}

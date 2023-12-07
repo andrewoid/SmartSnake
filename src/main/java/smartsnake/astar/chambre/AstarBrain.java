37663125b59f48f11e30cbb864cd8435ec36a6c0
chanacham <cchambre@student.touro.edu>
2023-12-06 19:11:25 -0500

    removed working comments

--- src/main/java/smartsnake/astar/chambre/AstarBrain.java
+++ src/main/java/smartsnake/astar/chambre/AstarBrain.java
@@ -11,40 +11,24 @@ import java.util.List;
 public class AstarBrain implements Brain {
     @Override
     public Direction move(Snake snake, Food food, Garden garden) {
-        //determine what direction the snake should move via implementing A*
-        //return direction snake should move
 
-        //Point A is snake.getHeadLocation()
         Node pointA = new Node(snake.getHeadLocation(), food);
-        //Point B is food
-
-        //Open List set of nodes to be evaluated
         List<Node> open = new ArrayList<>();
-        //Closed List set of nodes already evaluated
         List<Node> closed = new ArrayList<>();
-
-        //add the start node to open
         open.add(pointA);
 
-        //loop
         while (true) {
 
-            //current node = node in open with lowest f-cost
             Node current = getLowestCost(open);
-            //remove current from open
             open.remove(current);
-            //add current to closed
             closed.add(current);
 
             Point location = current.getLocation();
 
-            //IF current is the target node
             if (location.equals(food)) {
                 return current.getDirectionFromStart();
             }
 
-            //foreach neighbor of the current node
-            // if one of the neighbors in open then update the node to be that one
             Point upPoint = new Point(location.x, location.y - 1);
             updateDirection(upPoint, Direction.Up, food, open, closed, snake, current, garden);
             Point downPoint = new Point(location.x, location.y + 1);
@@ -70,16 +54,11 @@ public class AstarBrain implements Brain {
             if (open.contains(directionNode)) {
                 directionNode = open.get(open.indexOf(directionNode));
             }
-            //if the neighbor is not traversable or neighbor is in closed
             if (snake.getSegments().contains(directionNode.getLocation()) || closed.contains(directionNode)) {
                 return;
             }
-
-            //set parent of neighbour to current
             directionNode.setParent(current, direction);
-            //if neighbour is not in open
             if (!open.contains(directionNode)) {
-                //add neighbour to open
                 open.add(directionNode);
             }
         }

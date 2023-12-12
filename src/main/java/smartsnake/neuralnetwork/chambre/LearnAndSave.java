29ec7d37f43041c6e74066645a2af5fbe9c3a7a7
chanacham <cchambre@student.touro.edu>
2023-12-12 16:10:58 -0500

    checkstyles and bug fix

--- src/main/java/smartsnake/neuralnetwork/chambre/LearnAndSave.java
+++ src/main/java/smartsnake/neuralnetwork/chambre/LearnAndSave.java
@@ -9,35 +9,26 @@ import smartsnake.neuralnetwork.NeuralNetworkDataFactory;
 public class LearnAndSave {
     public static void main(String[] args) {
 
-        //Create a NN
-        //        5 input nodes
-        //        256 hidden nodes
-        //        4 output nodes
         NeuralNetwork neuralNetwork = new NeuralNetwork(5, 256, 4);
         NeuralNetworkDataFactory neuralNetworkDataFactory = new NeuralNetworkDataFactory();
 
-        //Play 50,000 games of Snake.
         for (int i = 0; i < 50_000; i++) {
-            //For every move, train the NN with the state of the Garden and the move that your AStarBrain decides to make.
             AstarBrain astarBrain = new AstarBrain();
             Garden garden = new Garden(20, 15);
             Snake snake = garden.getSnake();
             snake.setBrain(astarBrain);
 
             while (true) {
-                //train method needs two arrays
-                double[] gardenInput = neuralNetworkDataFactory.toInput(garden);
-                //call tick before training to save the state of the garden
+
                 boolean gardenTick = garden.tick();
-                neuralNetwork.train(
-                        gardenInput,
-                        neuralNetworkDataFactory.toOutput(snake.getDirection()));
+                double[] gardenInput = neuralNetworkDataFactory.toInput(garden);
+                double[] snakeOutput = neuralNetworkDataFactory.toOutput(snake.getDirection());
+                neuralNetwork.train(gardenInput, snakeOutput);
                 if (!gardenTick) {
                     break;
                 }
             }
         }
-        //     Save the NN
         neuralNetwork.writeToFile();
     }
 }

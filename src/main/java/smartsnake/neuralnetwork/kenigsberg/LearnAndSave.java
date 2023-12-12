4ca193b780c2ce79300af4cc13e4536d210b2160
lkenigsb <libbykenigsberg@gmail.com>
2023-12-12 13:22:49 -0500

    complete nn

--- src/main/java/smartsnake/neuralnetwork/kenigsberg/LearnAndSave.java
+++ src/main/java/smartsnake/neuralnetwork/kenigsberg/LearnAndSave.java
@@ -1,8 +1,6 @@
 package smartsnake.neuralnetwork.kenigsberg;
 
 import basicneuralnetwork.NeuralNetwork;
-import smartsnake.Brain;
-import smartsnake.BrainFactory;
 import smartsnake.Garden;
 import smartsnake.Snake;
 import smartsnake.astar.kenigsberg.AstarBrain;
@@ -12,16 +10,16 @@ import java.io.IOException;
 
 public class LearnAndSave {
     /**
-     *  1. Create a NN
-     *      a. 5 input nodes
-     *      b. 256 hidden nodes
-     *      c. 4 output nodes
-     *  2. Play 50,000 games
-     *      a. for(I=0; I<50,000; I++)
-     *          i. Have inner loop - while the game is still going, keep going (garden.tick() is true…)
-     *          ii. For every move, train the NN with the state of the Garden
-     *          & the move that your AstarBrain decides to make
-     *  3. Save the NN
+     * 1. Create a NN
+     * a. 5 input nodes
+     * b. 256 hidden nodes
+     * c. 4 output nodes
+     * 2. Play 50,000 games
+     * a. for(I=0; I<50,000; I++)
+     * i. Have inner loop - while the game is still going, keep going (garden.tick() is true…)
+     * ii. For every move, train the NN with the state of the Garden
+     * & the move that your AstarBrain decides to make
+     * 3. Save the NN
      */
     public static void main(String[] args) throws IOException {
 
@@ -30,19 +28,19 @@ public class LearnAndSave {
 
         NeuralNetworkDataFactory networkDataFactory = new NeuralNetworkDataFactory();
 
-        for (int i = 0; i < 50_000; i++)
-        {
+        for (int i = 0; i < 50_000; i++) {
             Garden garden = new Garden(20, 15);
             Snake snake = garden.getSnake();
             AstarBrain brain = new AstarBrain();
             snake.setBrain(brain);
 
-            // 3 things happen in this loop:
-            // call toInput, call tick, train
-            while (!snake.intersects(garden.getFood())) {
+            while (true) {
                 double[] input = networkDataFactory.toInput(garden);
-                garden.tick();
+                boolean ticked = garden.tick();
                 neuralNetwork.train(input, networkDataFactory.toOutput(snake.getDirection()));
+                if (!ticked) {
+                    break;
+                }
             }
         }
         neuralNetwork.writeToFile();

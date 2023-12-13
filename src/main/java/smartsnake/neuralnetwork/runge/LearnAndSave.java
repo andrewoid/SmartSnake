7fb9bea114595cfc5884de2ae9bd3a5e96f17a4e
emmyrunge <erunge@student.touro.edu>
2023-12-13 12:14:07 -0600

    wont update

--- src/main/java/smartsnake/neuralnetwork/runge/LearnAndSave.java
+++ src/main/java/smartsnake/neuralnetwork/runge/LearnAndSave.java
@@ -9,14 +9,10 @@ import smartsnake.neuralnetwork.NeuralNetworkDataFactory;
 public class LearnAndSave
 {
     public static void main(String[] args) {
-        //create a neural network
-        //5 input nodes
-        //256 hidden nodes
-        //4 output nodes
+
         NeuralNetwork neuralNetwork = new NeuralNetwork(5, 256, 4);
         NeuralNetworkDataFactory neuralNetworkDataFactory = new NeuralNetworkDataFactory();
 
-        //play 50_000 games
         for (int i = 0; i < 50_000; i++)
         {
             Garden garden = new Garden(20, 15);
@@ -24,17 +20,23 @@ public class LearnAndSave
             AstarBrain brain = new AstarBrain();
             snake.setBrain(brain);
 
-            //while snake can move
+            boolean ticked;
             do
             {
-                //for every move, train the NN with the state of the garden,
-                //and the move that your AstarBrain decides to make
-                neuralNetwork.train(neuralNetworkDataFactory.toInput(garden),
+                double[] input = neuralNetworkDataFactory.toInput(garden);
+                ticked = garden.tick();
+                neuralNetwork.train(input,
                         neuralNetworkDataFactory.toOutput(snake.getDirection()));
-            } while (garden.tick());
+            } while (ticked);
+
         }
-        //save the NN, found in NN class
-        //neuralNetwork.writeToFile();
         neuralNetwork.writeToFile();
     }
 }
+//create a neural network
+//5 input nodes
+//256 hidden nodes
+//4 output nodes
+//play 50_000 games
+//for every move, train the NN with the state of the garden,
+//and the move that your AstarBrain decides to make
